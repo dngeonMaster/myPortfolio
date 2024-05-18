0ece0738836412c531cb8224e2669f419db3e568
dngeonMasteer <kaushikborah4080@gmail.com>
2024-05-18 12:28:24 +0530

    First Commit

--- src/components/Techologies.jsx
+++ src/components/Techologies.jsx
@@ -6,42 +6,94 @@ import { FaHtml5 } from "react-icons/fa";
 import { FaBootstrap } from "react-icons/fa";
 import { GrMysql } from "react-icons/gr";
 import { FaPython } from "react-icons/fa";
+import { motion, transform } from "framer-motion"
 
 
-
+const iconVarinats=(duration)=>({
+    initial:{y:-10},
+    animate:{
+        y:[10,-10],
+        transition:{
+            duration:duration,
+            ease:"linear",
+            repeat:Infinity,
+            repeatType:"reverse",
+        }
+    }
+})
 
 const Techologies=()=> {
   return (
-    <div className="border-b border-neutral-800p pb-24">
-        <h1 className="my-20 text-center text-4xl">Technical Skills</h1>
-        <div className="flex flex-wrap items-center justify-center">
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+    <div className="border-b border-neutral-800 pb-24">
+        <motion.h1
+        whileInView={{opacity:1,y:0}}
+        initial={{opacity:0,y:-100}}
+        transition={{duration:1.5}}
+         className="my-20 text-center text-4xl">Technical Skills</motion.h1>
+        <motion.div
+        whileInView={{opacity:1, x:0}}
+        initial={{opacity:0,x:-100}}
+        transition={{duration:1.5}}
+         className="flex flex-wrap items-center justify-center gap-4">
+            <motion.div
+            variants={iconVarinats(2.5)}
+            initial="initial"
+            animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 p-4">
                 <RiReactjsLine className="text-7xl text-cyan-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+            variants={iconVarinats(3)}
+            initial="initial"
+            animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 p-4">
                 <FaNodeJs className="text-7xl text-green-500"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+            variants={iconVarinats(5)}
+            initial="initial"
+            animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 p-4">
                 <RiTailwindCssFill className="text-7xl text-blue-800"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+            variants={iconVarinats(2)}
+            initial="initial"
+            animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 p-4">
                 <TbBrandMongodb className="text-7xl text-green-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+            variants={iconVarinats(6)}
+            initial="initial"
+            animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 p-4">
                 <FaHtml5 className="text-7xl text-red-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+            variants={iconVarinats(4)}
+            initial="initial"
+            animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 p-4">
                 <FaBootstrap className="text-7xl text-cyan-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+            variants={iconVarinats(5.5)}
+            initial="initial"
+            animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 p-4">
                 <GrMysql className="text-7xl text-cyan-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+            variants={iconVarinats(6)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
                 <FaPython className="text-7xl text-yellow-300"/>
-            </div>
+            </motion.div>
 
            
-        </div>
+        </motion.div>
       
     </div>
   );
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,39 +3,40 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a BTech student at the National Institute of Technology, Silchar, graduating in 2025. With a strong foundation in engineering principles from NIT Silchar, I have cultivated a deep passion for coding, web development, machine learning, and problem-solving. My expertise spans across full stack development, including proficiency in HTML, CSS, JavaScript, React, Node.js, and databases like MongoDB and SQL. I thrive on creating innovative solutions and am dedicated to leveraging my skills to build efficient and impactful technologies.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `Beyond my coursework, I have actively engaged in various projects and internships that have allowed me to apply my knowledge to real-world scenarios. My current research internship focuses on integrating machine learning algorithms with web applications, further enhancing my technical expertise. I thrive on creating innovative solutions that blend advanced machine learning with seamless web interfaces, and I am dedicated to leveraging my skills to build efficient, impactful technologies.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "2021 - Present",
+    role: "National Institute of Technology, Silchar",
+    company: "BTech",
+    description: `
+    I am currently pursuing a BTech from the National Institute of Technology, Silchar, with an expected graduation in 2025.Alongside my academic pursuits, I have developed a passion for coding and development, actively enhancing my skills through various programming languages, projects, and hackathons. I am excited to merge my technical knowledge with my enthusiasm for software development to drive innovative solutions in the future.`,
+    technologies: ["Programming", "Problem Solving", "Development"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    year: "2024 - Present",
+    role: "Summer Research Intern",
+    company: "NIT Silchar",
+    description: `I am currently working as a research intern in the fields of Machine Learning and Web Development. In this role, I am focusing on applying advanced machine learning algorithms to real-world problems, while also developing and optimizing web applications to support these projects. My responsibilities include data analysis, model training and evaluation, and creating user-friendly web interfaces for seamless interaction with the developed models. `,
+    technologies: [ "FullStack Development","Machine Learning", "Data Science"],
   },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+  // {
+  //   year: "2021 - 2022",
+  //   role: "Full Stack Developer",
+  //   company: "Facebook",
+  //   description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+  //   technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  // },
+  // {
+  //   year: "2020 - 2021",
+  //   role: "Software Engineer",
+  //   company: "Paypal",
+  //   description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+  //   technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  // },
 ];
 
 export const PROJECTS = [
@@ -47,7 +48,7 @@ export const PROJECTS = [
     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
   },
   {
-    title: "Task Management App",
+    title: "TaskIt",
     image: project2,
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
@@ -58,10 +59,10 @@ export const PROJECTS = [
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["HTML", "CSS", "React", "Tailwind","FramerMotion"],
   },
   {
-    title: "Blogging Platform",
+    title: "BlogME",
     image: project4,
     description:
       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
@@ -70,7 +71,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "National Institute of Techhnology,Silchar",
+  phoneNo: "+91-600-040-5413 ",
+  email: "kaushikborah4080@gmail.com",
 };
